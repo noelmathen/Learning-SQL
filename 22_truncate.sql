e6a2af176e3b0d6b29fb1369f54160410fb730f9
Noel Mathen <140234564+noelmathen@users.noreply.github.com>
2025-01-16 22:39:20 +0530

    truncate

---
+++ 22_truncate.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE student;
+
+
+select * from student;
+
+INSERT INTO student (rollno, name, marks, grade, city)
+VALUES
+    (101, "anil", 78, "C", "Pune"),
+    (102, "bhumika", 93, "A", "Mumbai"),
+    (103, "chetan", 85, "B", "Mumbai"),
+    (104, "dhruv", 96, "A", "Delhi"),
+    (105, "emanuel", 12, "F", "Delhi"),
+    (106, "farah", 82, "B", "Delhi");
